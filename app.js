8ad67da8b01dc06a24ec4bbb1e54926d6207da3a
camariana <camariana40@outlook.com>
2021-05-20 16:14:44 +0000

    make it like the one in the course material

--- app.js
+++ app.js
@@ -23,11 +23,17 @@ mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology
   })
 
 
-
+app.use(express.static('build'))
 app.use(express.json())
 app.use(cors())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms'))
+app.use(middleware.requestLogger)
+
 
 app.use('/api/blogs', blogsRouter)
+
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
 
 module.exports = app
