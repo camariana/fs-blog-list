cf7fd05c153f89eef20909cfd684c02d9b428822
camariana <camariana40@outlook.com>
2021-05-22 09:22:29 +0000

    helper functions and unit tests, step2

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,7 +1,14 @@
 const dummy = (blogs) => {
-  // ...
+  return blogs.length
+}
+
+const totalLikes = (blogs) => {
+  return blogs.reduce((acc, cur) => {
+    return acc + cur.likes
+  }, 0)
 }
 
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
