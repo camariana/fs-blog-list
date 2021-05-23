97ccf59195ab620ae24dfe6d0010e08fa1f8504e
camariana <camariana40@outlook.com>
2021-05-23 16:00:51 +0000

    Blog list tests, step3

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -1,8 +1,18 @@
-const mongoose = require('mongoose')
 const supertest = require('supertest')
+const mongoose = require('mongoose')
+const helper = require('./test_helper')
 const app = require('../app')
-
 const api = supertest(app)
+
+const Blog = require('../models/blog')
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+
+  let blogObjects = helper.initialBlogs.map(blog => new Blog(blog))
+  const promiseArray = blogObjects.map(blog => blog.save())
+  await Promise.all(promiseArray) // wait for all of the asynchronous operations to finish executing with the Promise.all method
+})
 
 test('blogs are returned as json', async () => {
   await api
@@ -16,6 +26,29 @@ test('verify that the _id is named id', async () => {
 
   const contents = response.body.map(r => r.id)
   expect(contents).toBeDefined()
+})
+
+test('verify success of new blog creation', async () => {
+  const newBlog = {
+    title: 'What we are living for',
+    author: 'A. Camariana',
+    url: 'camariana.gm',
+    likes: 0,
+  }
+
+  await api
+    .post('/api/blogs/')
+    .send(newBlog)
+    .expect('Content-Type', /application\/json/)
+
+  const response = await api.get('/api/blogs')
+
+  const contents = response.body.map(r => r.title)
+
+  expect(response.body).toHaveLength(helper.initialBlogs.length + 1)
+  expect(contents).toContain(
+    'What we are living for'
+  )
 })
 
 afterAll(() => {
---
+++ tests/test_helper.js
@@ -0,0 +1,79 @@
+const Blog = require('../models/blog')
+
+// array containing the initial database state
+const initialBlogs = [
+  {
+    title: 'An Open Letter to Jason and David',
+    author: 'Jane Yang',
+    url: 'https://janeyang.org/2021/04/27/an-open-letter-to-jason-and-david/',
+    likes: 3
+  },
+  {
+    title: 'Which jobs help people the most?',
+    author: 'Benjamin Todd',
+    url: 'https://80000hours.org/career-guide/high-impact-jobs/#approach-1-earning-to-give',
+    likes: 2
+  },
+  {
+    title: 'React patterns',
+    author: 'Michael Chan',
+    url: 'https://reactpatterns.com/',
+    likes: 7
+  },
+  {
+    title: 'Go To Statement Considered Harmful',
+    author: 'Edsger W. Dijkstra',
+    url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+    likes: 5
+  },
+  {
+    title: 'Canonical string reduction',
+    author: 'Edsger W. Dijkstra',
+    url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
+    likes: 12
+  },
+  {
+    title: 'First class tests',
+    author: 'Robert C. Martin',
+    url: 'http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll',
+    likes: 10
+  },
+  {
+    title: 'TDD harms architecture',
+    author: 'Robert C. Martin',
+    url: 'http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html',
+    likes: 0
+  },
+  {
+    title: 'Type wars',
+    author: 'Robert C. Martin',
+    url: 'http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html',
+    likes: 2
+  }
+]
+
+// used for creating a database object ID that does not belong to any blog object in the database.
+const nonExistingId = async () => {
+  const blog = new Blog({
+    title: 'will remove this soon',
+    author: 'A. Camariana',
+    url: 'camariana.gm',
+    likes: 0
+  })
+  await blog.save()
+  await blog.remove()
+
+  return blog._id.toString()
+}
+
+// used for checking the blogs stored in the database
+const blogsInDb = async () => {
+  const blogs = await Blog.find({})
+  return blogs.map(blog => blog.toJSON())
+}
+
+module.exports = {
+  initialBlogs,
+  nonExistingId,
+  blogsInDb
+}
